15b2c7b2b80b682a773d485b4f71f745a1d5a014
Fer <weddinginvitationscampfire@gmail.com>
2026-04-16 13:55:49 +0200

    fix: update Stripe API version to 2025-02-24.acacia

--- src/lib/stripe/stripe.ts
+++ src/lib/stripe/stripe.ts
@@ -10,7 +10,7 @@ if (!STRIPE_SECRET_KEY || STRIPE_SECRET_KEY.includes('REEMPLAZA')) {
 }
 
 export const stripe = new Stripe(STRIPE_SECRET_KEY, {
-  apiVersion: '2024-11-20.acacia',
+  apiVersion: '2025-02-24.acacia',
   typescript: true,
 });
 
